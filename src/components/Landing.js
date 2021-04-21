d0449889486ac3a3db266dc0ab6bcd877c047a6b
KareemZuwa <kareemzuwa@gmail.com>
2021-04-21 16:09:00 +0200

    floating action button på set alarm

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/svg/Logo.svg';
 import welcome from '../assets/svg/Welcome.svg';
+import Fab from '@material-ui/core/Fab';
 
 function Landing() {
     return (
@@ -9,7 +10,11 @@ function Landing() {
             <img className='welcome'src={welcome} alt='welcome' />
             <img src={logo} alt='logotype' />
 
-            <Link to='/'>Set Alarm</Link>
+
+        
+            <Link to='/'>
+            <Fab className='set-alarm-button' variant="contained" color="primary">Set Alarm</Fab>
+            </Link>
 
             
         </section>
@@ -17,3 +22,14 @@ function Landing() {
 }
 
 export default Landing
+
+/* import Button from '@material-ui/core/Button';
+
+function App() {
+return (
+<Button variant="contained" color="primary">
+Hello World
+</Button>
+);
+}
+*/

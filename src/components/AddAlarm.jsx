741bbc0b9cdc1c1ee003d071a9dda6eac7902ce6
NooraAli20 <noora_ali89@hotmail.com>
2021-04-21 18:16:52 +0200

    new file

---
+++ src/components/AddAlarm.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      width: '100%',
+    },
+    heading: {
+      fontSize: theme.typography.pxToRem(15),
+      fontWeight: theme.typography.fontWeightRegular,
+    },
+  }));
+  
+export default function AddAlarm() {
+    const classes = useStyles();
+    return (
+        <div>
+            
+        </div>
+    )
+}

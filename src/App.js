35687ce72586ca8bb92471ceb7e03c4990ae47b2
elisabethdeborah <elisabeth_deborah@hotmail.com>
2021-04-21 15:11:14 +0200

    Förslag på states

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Landing from './components/Landing';
 
+
+
 function App() {
+  //# ALARM-SIDAN (ELISABETH)
+  //OFF-BTN-STATUS ÄR FALSE TILLS MAN KLICKAR, SNOOZE-BTN-STATUS ÄR FALSE TILLS MAN KLICKAR
+  const [offbtn, setOffBtn] = useState(false);
+  const [snoozebtn, setSnoozeBtn] = useState(false);
+
+  //SIDA #4, SET-ALARM PAGE (NOORA)
+  //BESTÄMMER TID FÖR ALARM, BESTÄMMER VAL AV LJUD FÖR ALARM, BESTÄMMER OM SNOOZE SKA FINNAS OCH VILKEN TIDSINTERVALL
+  const [alarmtime, setAlarmTime] = useState(0);
+  const [alarmsound, setAlarmSound] = useState('');
+  const [snoozesime, setSnoozeTime] = useState(5);
+
+
   return (
     <div className="App">
       <Switch>

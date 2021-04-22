0fb54be28dcd0ce00e1095b4973cf2f55abf5193
elisabethdeborah <elisabeth_deborah@hotmail.com>
2021-04-22 12:43:48 +0200

    fixat text på slider

--- src/components/OffBtn.js
+++ src/components/OffBtn.js
@@ -12,56 +12,61 @@ const OffBtn = () => {
         root: {
           width: 300 + theme.spacing(3) * 2,
         },
-        margin: {
-          height: theme.spacing(3),
-        },
       }));
-    const PrettoSlider = withStyles({
+
+      const AirbnbSlider = withStyles({
         root: {
-          color: '#877689',
-          width: 195,
-          position: 'relative',
-          paddingBottom: 100,
-        },
-        thumb: {
-          height: 90,
-          width: 90,
-          backgroundColor: '#F7D859',
-          border: '2px solid #ffffff',
-          marginTop: -5,
-          marginLeft: -12,
-          paddingRight: 24,
-          '&:focus, &:hover, &$active': {
-            boxShadow: 'inherit',
+            color: '#877689',
+            width: 195,
+            position: 'relative',
+            paddingBottom: 100,
           },
-        },
-        active: {},
-        valueLabel: {
-          left: 'calc(-50% + 4px)',
-        },
-        track: {
-          height: 70,
-          borderTopLeftRadius: 50,
-          borderBottomLeftRadius: 50,
-          paddingRight: 10,
-          border: '5px solid #ffffff',
-        },
-        rail: {
-            
-          height: 70,
-          borderRadius: 50,
-          border: '5px solid #ffffff',
-        },
+          thumb: {
+            color: '#000000',
+            fontSize: 35,
+            height: 90,
+            width: 90,
+            backgroundColor: '#F7D859',
+            border: '2px solid #ffffff',
+            marginTop: -5,
+            marginLeft: -12,
+            '&:focus, &:hover, &$active': {
+              boxShadow: 'inherit',
+            },
+          },
+          active: {},
+          valueLabel: {
+            left: 'calc(-50% + 4px)',
+          },
+          track: {
+            height: 70,
+            borderTopLeftRadius: 50,
+            borderBottomLeftRadius: 50,
+            paddingRight: 10,
+            border: '5px solid #ffffff',
+          },
+          rail: {
+            height: 70,
+            borderRadius: 50,
+            border: '5px solid #ffffff',
+          },
       })(Slider);
-
-
+      
+      function AirbnbThumbComponent(props) {
+          console.log(props)
+        return (
+          <span {...props}>
+              Off
+          </span>
+        );
+      }
         const classes = useStyles();
       
         return (
           <div className={classes.root}>   
-            <Typography gutterBottom>OFF</Typography>
-            <PrettoSlider valueLabelDisplay="auto" aria-label="pretto slider" defaultValue={0} />
-            <div className={classes.margin} />
+            <AirbnbSlider
+                ThumbComponent={AirbnbThumbComponent}
+            />
           </div>
         );
 

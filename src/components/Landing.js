7f492b939dc9986d6acab8d03f725df86db74c8c
KareemZuwa <kareemzuwa@gmail.com>
2021-04-22 10:13:31 +0200

    landing page styled and done

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,34 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/svg/Logo.svg';
 import welcome from '../assets/svg/Welcome.svg';
-import Fab from '@material-ui/core/Fab';
 
 function Landing() {
     return (
         <section className='landing-wrapper'>
-            <img className='welcome'src={welcome} alt='welcome' />
-            <img src={logo} alt='logotype' />
+            <img className='welcome' src={welcome} alt='welcome' />
+            <img className='logotype' src={logo} alt='logotype' />
 
-
-        
-            <Link to='/addalarm'>
-            <Fab className='set-alarm-button' variant="contained" color="primary">Set Alarm</Fab>
-            </Link>
-
-            
+            <Link to='/'>Set alarm</Link>
         </section>
     )
 }
 
 export default Landing
-
-/* import Button from '@material-ui/core/Button';
-
-function App() {
-return (
-<Button variant="contained" color="primary">
-Hello World
-</Button>
-);
-}
-*/
